8f51ca3dc20838abfb535b732103d64e14d9cfb5
iSmartCoding <ismartcoding@gmail.com>
2023-05-14 23:33:03 +0800

    update version to 1.0.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,9 +36,9 @@ android {
             else -> 0
         }
 
-        val vCode = 30
+        val vCode = 33
         versionCode = vCode - singleAbiNum
-        versionName = "1.0.20"
+        versionName = "1.0.21"
 
         ndk {
             abiFilters += abiFilterList
