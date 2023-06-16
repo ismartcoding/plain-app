f5b16804166346cb0785c410fb71018f9ff979d6
iSmartCoding <ismartcoding@gmail.com>
2023-06-16 09:33:33 +0800

    Upgrade version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,9 +34,9 @@ android {
             else -> 0
         }
 
-        val vCode = 43
+        val vCode = 46
         versionCode = vCode - singleAbiNum
-        versionName = "1.0.24"
+        versionName = "1.0.25"
 
         ndk {
             abiFilters += abiFilterList
