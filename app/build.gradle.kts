df9fab5160805fd56cf2df3f1b77038bc100247d
iSmartCoding <ismartcoding@gmail.com>
2023-07-04 14:40:09 +0800

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,9 +34,9 @@ android {
             else -> 0
         }
 
-        val vCode = 67
+        val vCode = 70
         versionCode = vCode - singleAbiNum
-        versionName = "1.1.1"
+        versionName = "1.1.2"
 
         ndk {
             if (abiFilterList.isNotEmpty()) {
