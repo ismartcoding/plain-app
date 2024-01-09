b4dc3ccd91466dd914c13f9c4b6466b9e5851227
iSmartCoding <ismartcoding@gmail.com>
2024-01-09 22:38:05 +0800

    Update web code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,9 +37,9 @@ android {
                 else -> 0
             }
 
-        val vCode = 211
+        val vCode = 214
         versionCode = vCode - singleAbiNum
-        versionName = "1.2.26"
+        versionName = "1.2.27"
 
         ndk {
             abiFilters += abiFilterList.ifEmpty {
