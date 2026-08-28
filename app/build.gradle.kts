55e9b5eb4c74803be69b2d430c6024ab13a1f4b2
iSmartCoding <ismartcoding@gmail.com>
2026-08-28 12:22:46 +0800

    fix: keep getIdentifier-looked-up resources from release shrinker, bump 3.3.9 (658)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,9 @@ android {
                 else -> 0
             }
 
-        val vCode = 655
+        val vCode = 658
         versionCode = vCode - singleAbiNum
-        versionName = "3.3.8"
+        versionName = "3.3.9"
 
         ndk {
             //noinspection ChromeOsAbiSupport
