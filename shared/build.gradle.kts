f92e7d8ad0f8d56ba4090ff134525350ccc3b094
iSmartCoding <ismartcoding@gmail.com>
2026-09-01 23:44:10 +0800

    refactor: vendor ktor-server-conditional-headers sources into shared-lib

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -108,7 +108,6 @@ kotlin {
             implementation(libs.ktor.server.cors)
             implementation(libs.ktor.server.forwarded.header)
             implementation(libs.ktor.server.partial.content)
-            implementation(libs.ktor.server.conditional.headers)
 
             // Media3 (AudioPlayerService, audio playback, PlayerView UI)
             implementation(libs.media3.exoplayer)
--- shared/src/androidMain/kotlin/com/ismartcoding/plain/httpserver/HttpModule.kt
+++ shared/src/androidMain/kotlin/com/ismartcoding/plain/httpserver/HttpModule.kt
@@ -19,7 +19,7 @@ import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.http.content.staticResources
 import com.ismartcoding.plain.lib.ktorserver.AutoHeadResponse
-import io.ktor.server.plugins.conditionalheaders.ConditionalHeaders
+import com.ismartcoding.plain.lib.ktorserver.ConditionalHeaders
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.plugins.cors.routing.CORS
 import io.ktor.server.plugins.forwardedheaders.ForwardedHeaders
